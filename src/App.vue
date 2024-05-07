06def32c4a6172d92d4bf9bdb1d61dc2e3113e5c
alec <alecrondel@hotmail.com>
2024-05-07 10:22:37 -0300

    improving navbar and adding navigation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <RouterLink to="/">Notes</RouterLink>
-  <RouterLink to="/stats">Stats</RouterLink>
-
+  <!-- <RouterLink to="/">Notes</RouterLink>
+  <RouterLink to="/stats">Stats</RouterLink> -->
+  <NavBar />
   <RouterView />
 </template>
+
+<script setup>
+
+  import NavBar from './components/Layout/NavBar.vue';
+
+
+
+</script>
 
 <style>
   @import url(node_modules\bulma\css\bulma.min.css);
