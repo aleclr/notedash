499dd885d30851ed8d0d52cc79de6a1eb9c85068
Alec <alecrondel@hotmail.com>
2024-05-27 22:09:05 -0300

    adding real time listening and updating notes

--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -1,5 +1,5 @@
 import { defineStore, mapActions } from 'pinia';
-import { collection, getDocs } from "firebase/firestore";
+import { collection, onSnapshot } from "firebase/firestore";
 import { db } from '@/js/firebase';
 
 export const useStoreNotes = defineStore('storeNotes', {
@@ -20,13 +20,26 @@ export const useStoreNotes = defineStore('storeNotes', {
   actions: {
 
     async getNotes() {
-      const querySnapshot = await getDocs(collection(db, "notes"));
-      querySnapshot.forEach((doc) => {
-        let note = {
-          id: doc.id,
-          content: doc.data().content
-        };
-        this.notes.push(note);
+      // const querySnapshot = await getDocs(collection(db, "notes"));
+      // querySnapshot.forEach((doc) => {
+      //   let note = {
+      //     id: doc.id,
+      //     content: doc.data().content
+      //   };
+      //   this.notes.push(note);
+      // });
+      onSnapshot(collection(db, 'notes'), (querySnapshot) => {
+        let notes = [];
+
+        querySnapshot.forEach((doc) => {
+          let note = {
+            id: doc.id,
+            content: doc.data().content
+          };
+          notes.push(note);
+        });
+
+        this.notes = notes;
       });
     },
 
